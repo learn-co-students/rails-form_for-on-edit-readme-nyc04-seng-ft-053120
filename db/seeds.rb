8f70c8e69f54ea8ce0dea0c0926773739482fc9c
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-21 00:12:10 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create([
+    {
+        title: "Uptown Girl",
+        description: "Song by Billy Joel"
+    }
+])
